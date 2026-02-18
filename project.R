a3f7e9394bd458bc2b426a139e1f0e511344342a
Grace Rost <rost.grace@gmail.com>
2026-02-18 14:09:58 -0800

    adding population data

--- project.R
+++ project.R
@@ -9,3 +9,7 @@ countries <- read.csv("olympics_medals_country_wise.csv", header=TRUE, sep=",")
 gdp <- read.csv("gdp_data.csv", header=TRUE, sep=",")
 
 head(gdp)
+
+population <- read.csv("population.csv", header=TRUE, sep=",")
+
+head(population)
