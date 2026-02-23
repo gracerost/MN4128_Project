dcc9bfa573046b24a72b00ec7282564a8ea5cbf1
Grace Rost <rost.grace@gmail.com>
2026-02-23 14:31:34 -0800

    more updates to gdp_pop

--- project.R
+++ project.R
@@ -10,18 +10,15 @@ library(rio)
 library(foreign)
 library(haven)
 
-#read in the olympic athlete dataset
-athletes <- read.csv("athlete_events.csv", header=TRUE, sep=",")
 
-#table(athletes$)
-countries <- read.csv("olympics_medals_country_wise.csv", header=TRUE, sep=",")
+################################################
+# First task will be creating a dataset that displays the country, year, GDP, Population, and GDP per Capita
 
+#read in the GDP dataset
 gdp <- read.csv("gdp_data.csv", header=TRUE, sep=",")
 
-#remove nas from gdp
+#remove blanks from gdp
 gdp <- filter(gdp, Country.Code != "")
-
-head(gdp)
 
 #create a function that take a substring of the complex year variable and returns just the year
 reformat_yearvar <- function(var){
@@ -32,20 +29,17 @@ reformat_yearvar <- function(var){
 #rename year variables to improve readability
 gdp <- rename_with(gdp, .fn = reformat_yearvar, .cols= contains("YR"))
 
-
-head(gdp)
-
 #read in the population data
 population <- read.csv("population.csv", header=TRUE, sep=",")
+
+#remove blanks from population
 population <- filter(population, Country.Code != "")
 
 #rename year variables to improve readability
 population <- rename_with(population, .fn = reformat_yearvar, .cols= contains("YR"))
 
-head(population)
-
 #reshape the gdp dataset to long format
-long_gdp <- reshape(gdp, idvar="Country.Code", varying= list(5:69), timevar="year", times= names(gdp)[5:69], direction="long")
+long_gdp <- reshape(gdp, idvar="Country.Code", varying= list(6:70), timevar="year", times= names(gdp)[5:69], direction="long")
 long_gdp <- rename(long_gdp, "GDP"="1960")
 
 #reshape the population dataset to long format
@@ -55,11 +49,26 @@ long_pop <- rename(long_pop, "Population"="1960")
 #merge the datasets - this should drop the years 2025 and 2026
 gdp_pop <- merge(long_gdp, long_pop, by=c("Country.Code", "Country.Name", "year"))
 
-table(gdp_pop$year)
-
+#create a new variable with GDP per Capita
 gdp_pop <- gdp_pop %>%
   mutate(
+    GDP = na_if(GDP, ".."),
     gdp_per_cap = as.numeric(GDP)/as.numeric(Population)
     )
 
-head(gdp_pop)
+#filter out the region observations from the dataset
+gdp_pop <- filter(gdp_pop, Type=="Country")
+
+######################################
+# Second Task will be creating a dataset that has the country, year, and medals won
+
+#read in the olympic athlete dataset
+athletes <- read.csv("athlete_events.csv", header=TRUE, sep=",")
+
+#table(athletes$)
+# countries <- read.csv("olympics_medals_country_wise.csv", header=TRUE, sep=",")
+
+#create a new dataset that only includes medalists
+medalists <- filter(athletes, !is.na(Medal))
+
+#
